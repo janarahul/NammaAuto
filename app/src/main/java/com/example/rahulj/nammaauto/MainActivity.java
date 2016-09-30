3b9ae89003141b71cfaad3dbc3cc2fdcf5c6f3b9
janarahul <janarahul123@outlook.com>
2016-09-30 10:05:58 +0530

    adding autocompleteplace fragment

--- app/src/main/java/com/example/rahulj/nammaauto/MainActivity.java
+++ app/src/main/java/com/example/rahulj/nammaauto/MainActivity.java
@@ -1,13 +1,89 @@
 package com.example.rahulj.nammaauto;
 
+import android.content.res.Resources;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.Spanned;
+import android.text.TextUtils;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+
+public class MainActivity extends SampleActivityBase implements PlaceSelectionListener {
+
+
+    int c=0;
+    CharSequence origin;
+    CharSequence dest;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        // Retrieve the PlaceAutocompleteFragment.
+        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+
+        // Register a listener to receive callbacks when a place has been selected or an error has
+        // occurred.
+        autocompleteFragment.setHint("Enter Origin");
+        autocompleteFragment.setOnPlaceSelectedListener(this);
+
+        PlaceAutocompleteFragment autocompleteFragment1 = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.autocomplete_fragment1);
+
+        autocompleteFragment1.setHint("Enter destination");
+        autocompleteFragment1.setOnPlaceSelectedListener(this);
+
+
+
+
     }
+
+    /**
+     * Callback invoked when a place has been selected from the PlaceAutocompleteFragment.
+     */
+    @Override
+    public void onPlaceSelected(Place place) {
+
+        if(c==0) {
+            Log.i(TAG, "Place Selected: " + place.getName());
+
+            origin=place.getName();
+            c++;
+        }
+        else {
+            Log.i(TAG, "Place Selected: " + place.getName());
+
+            dest=place.getName();
+            c=0;
+
+
+        }
+
+
+
+    }
+
+    /**
+     * Callback invoked when PlaceAutocompleteFragment encounters an error.
+     */
+    @Override
+    public void onError(Status status) {
+        Log.e(TAG, "onError: Status = " + status.toString());
+
+        Toast.makeText(this, "Place selection failed: " + status.getStatusMessage(),
+                Toast.LENGTH_SHORT).show();
+    }
+
+
 }
