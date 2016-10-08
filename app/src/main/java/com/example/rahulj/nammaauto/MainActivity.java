c987317e1c7219ea0268e50593c960597ccef387
janarahul <janarahul123@outlook.com>
2016-10-08 12:29:53 +0530

    Added notification feature

--- app/src/main/java/com/example/rahulj/nammaauto/MainActivity.java
+++ app/src/main/java/com/example/rahulj/nammaauto/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.rahulj.nammaauto;
 
 import android.*;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.graphics.Color;
 import android.net.Uri;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
@@ -123,6 +130,9 @@ public class MainActivity extends SampleActivityBase implements OnMapReadyCallba
 
 
 
+
+
+
     }
 
     private void sendRequest() {
@@ -215,7 +225,7 @@ public class MainActivity extends SampleActivityBase implements OnMapReadyCallba
         destinationMarkers = new ArrayList<>();
         Integer x;
         Float y;
-        float fare;
+        float fare=0;
 
 
         for (Route route : routes) {
@@ -238,6 +248,33 @@ public class MainActivity extends SampleActivityBase implements OnMapReadyCallba
                 fare= (float) (25+ y*.013);
                 ((TextView) findViewById(R.id.tvDuration)).setText("\u20B9"+fare);
             }
+            NotificationCompat.Builder mBuilder =
+                    new NotificationCompat.Builder(this)
+                            .setSmallIcon(R.drawable.common_google_signin_btn_icon_dark)
+                            .setContentTitle("Hi! This is NammaAuto").setSound(Settings.System.DEFAULT_NOTIFICATION_URI)
+                            .setContentText("Estimated trip fare is:"+"\u20B9"+fare);
+// Creates an explicit intent for an Activity in your app
+            Intent resultIntent = new Intent(String.valueOf(this));
+
+// The stack builder object will contain an artificial back stack for the
+// started Activity.
+// This ensures that navigating backward from the Activity leads out of
+// your application to the Home screen.
+            TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+// Adds the back stack for the Intent (but not the Intent itself)
+
+// Adds the Intent that starts the Activity to the top of the stack
+            stackBuilder.addNextIntent(resultIntent);
+            PendingIntent resultPendingIntent =
+                    stackBuilder.getPendingIntent(
+                            0,
+                            PendingIntent.FLAG_UPDATE_CURRENT
+                    );
+            mBuilder.setContentIntent(resultPendingIntent);
+            NotificationManager mNotificationManager =
+                    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+// mId allows you to update the notification later on.
+            mNotificationManager.notify(1, mBuilder.build());
 
 
 
